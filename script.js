7e6c6f313c85ee69de6009f103dea9fa1fd55736
Yogi 2911 <yogeswariyogi92@gmail.com>
2026-05-25 13:00:34 +0530

    Updated portfolio message

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function showMessage() {
     document.getElementById("msg").innerHTML =
-    "Welcome to my portfolio website 🚀";
+    "Hi, I'm Yogeswari 👋<br><br>I am an aspiring Cloud and DevOps Engineer with knowledge in AWS, Linux, Git, and web deployment. I enjoy building projects, learning new cloud technologies, and improving my technical skills every day. Currently, I am exploring AWS services like EC2, S3, IAM, and DevOps tools to grow my career in Cloud Computing.";
 }
